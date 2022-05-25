dee1d8635299f36b59dcf1120391197986ed2cfb
milfordestayschiller <103213100+milfordestayschiller@users.noreply.github.com>
2022-05-25 09:09:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,112 +1,123 @@
-//Se identifican los ids y la clase square
-
-var message = document.getElementById("message")
-var titulo = document.getElementById("titulo")
-var square = document.querySelectorAll(".square");
-var valorTexto = document.getElementById("colorDisplay")
-
-var playAgain = document.getElementById("playAgain")
-//Se asigna a la variable randomColor
-var reset = document.querySelector(".reset")
-var easy = document.getElementById("easy")
-var ultimosTres = document.getElementById("ultimosTres")
-valorTexto.innerHTML = randomColor()
 
 
-//Funcion que sirve para recorrer los cuadrados de la posicion 0 a 5 , ademas utiliza la funcion randomColor para hacer las comparaciones correspondientes y que el color aleatorio RGB del h1 sea coincidente con uno de los cuadrados, para posteriormente convierta a todos los demas cuadrados en el color que se selecciono como ganador.
 
+let colors = generateRandomColors()
+let cuadrados = document.querySelectorAll(".square")
+let message = document.querySelector("#message")
+let reset = document.querySelector("#reset")
+let easy = document.querySelector("#easy")
 
-function randomGlobal(valor) {
-
-   let x = Math.floor(Math.random() * square.length)
-   //esta arojando el mismo color ganador en otra posicion se repite
-   console.log(x)
-   for (let i = 0; i < square.length; i++) {
-
-      if (square[i].style.background = randomColor()) {
-
-         square[x].style.background = valorTexto.innerHTML;
-
-      }
-
+function requestFunction(params) {
+   for (let i = 0; i < cuadrados.length; i++) {
+      cuadrados[i].style.background = colors[i]
+      console.log(cuadrados[i])
    }
 
 }
 
+requestFunction()
 
-randomGlobal()
+let rgb = document.querySelector("#rgb")
+let pickedColor = pickColor()
+//console.log(colors[3])
+rgb.textContent = pickedColor
 
+function comparative(params) {
 
+   for (let i = 0; i < cuadrados.length; i++) {
+      cuadrados[i].addEventListener('click', function (event) {
+         let valorClick = cuadrados[i]
+         if (valorClick.style.backgroundColor == pickedColor) {
+            console.log("gano")
+            message.textContent = "Correcto"
+            rgb.style = `background: ${valorClick.style.backgroundColor} `
+            changeColors(valorClick.style.backgroundColor)
+            reset.textContent = "Play Again?"
+          }
+         else {
+            console.log("perdio")
+            valorClick.style.backgroundColor = "#232323"
+            message.textContent = "Intentelo Nuevamente"
+         }
+      }
 
-// Funcion para dar el color random en el rgb  en h1
+      )
+   }
+}
+comparative()
+
+function changeColors(color) {
+   for (let i = 0; i < cuadrados.length; i++) {
+      cuadrados[i].style.backgroundColor = color
+   }
+}
+
+changeColors()
+
+function pickColor(params) {
+   let rand = Math.floor(Math.random() * colors.length)
+   let arreglo = colors[rand]
+   return arreglo
+}
+pickColor()
+
 function randomColor() {
    var color1 = Math.floor(Math.random() * 256)
    var color2 = Math.floor(Math.random() * 256)
    var color3 = Math.floor(Math.random() * 256)
    return "rgb(" + color1 + ", " + color2 + ", " + color3 + ")"
+
 }
 
 randomColor()
 
+function generateRandomColors() {
+   let nuevosColores = []
 
-function comparaciones1(params) {
-
-
-   for (let i = 0; i < square.length; i++) {
-
-      square[i].addEventListener("click", function () {
-         //console.log(square[i].style.backgroundColor)
-
-         if (square[i].style.background == valorTexto.innerHTML) {
-
-            message.textContent = "Ganaste"
-            playAgain.style = "display:block"
-
-            reset.style = "visibility: hidden"
-
-
-
-
-            for (i = 0; i < square.length; i++) {
-               square[i].style.background = valorTexto.innerHTML
-            }
-
-         }
-
-         else {
-            message.textContent = "perdiste"
-            square[i].style = "background-color:#232323;"
-         }
-
-      })
-
+   for (var i = 0; i < 6; i++) {
+      nuevosColores[i] = randomColor()
    }
-
+   return nuevosColores
 }
 
-comparaciones1()
+function reset2(params) {
+   for (var i = 0; i < cuadrados.length; i++) {
+      if (cuadrados[i]) {
+         cuadrados[i].style.background = colors[i]
+         cuadrados[i].style.display = "block"
+      } else {
+         cuadrados[i].style.display = "none"
+      }
+   }
+   rgb.style.background = "steelblue"
+   reset.textContent = "Nuevos Colores"
+   message.textContent = ""
+}
 
-reset.addEventListener("click", function (params) {
+function resetear() {
+   reset.addEventListener("click", function (params) {
+      colors = generateRandomColors()
+      pickedColor = pickColor()
+    rgb.textContent = pickedColor
+      reset2()
+   })
+}
 
-  location.reload()
-
-})
-
-
-
-playAgain.addEventListener("click", function (params) {
-   randomGlobal()
-   comparaciones1()
-
-})
-
-
-
+resetear()
 easy.addEventListener("click", function (params) {
-   easy.onreset
-   randomGlobal()
+   colors = generateRandomColors(3)
+   pickedColor = pickColor()
+   rgb.textContent = pickedColor
+   reset2()
+   for (let i = 0; i < cuadrados.length - 3; i++) {
+      if (cuadrados[i]) {
+         cuadrados[i].style.display = "none"
+         cuadrados[i].style.background = colors[i]
 
-
-
+      }
+      else {
+         console.log("fuera de rango")
+      }
+   }
 })
 
